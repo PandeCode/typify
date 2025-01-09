67535bc33e28a9482e7458c6ea75ae96c851e59f
PandeCode <pandeshawnbenjamin@gmail.com>
2025-01-09 11:10:35 +0000

    redirect uri

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -70,7 +70,7 @@ pub fn get_spotify() -> AuthCodeSpotify {
             "user-read-currently-playing"
         ),
         state: get_state(),
-        redirect_uri: get_href(),
+        redirect_uri: web_sys::window().unwrap().location().origin().unwrap() + "/",
         ..Default::default()
     };
 
